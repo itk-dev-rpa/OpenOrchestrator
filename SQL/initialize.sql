b6d8e17ebc46fb89ea5098b7c8adca04facd88f4
Mathias G <ghbm@aarhus.dk>
2023-07-04 11:28:10 +0200

    Initialize sql tables added

---
+++ SQL/initialize.sql
@@ -0,0 +1,62 @@
+CREATE TABLE Log_Level (
+    id          TINYINT         NOT NULL    PRIMARY KEY,
+    level_text  VARCHAR(10)     NOT NULL
+);
+
+CREATE TABLE Logs (
+    id              UNIQUEIDENTIFIER    NOT NULL    PRIMARY KEY,
+    log_time        DATETIME            NOT NULL,
+    log_level       TINYINT             NOT NULL    FOREIGN KEY REFERENCES Log_Level(id),
+    process_name    VARCHAR(100)        NOT NULL,
+    log_message     VARCHAR(8000)
+);
+
+CREATE TABLE Trigger_Status (
+    id              TINYINT         NOT NULL    PRIMARY KEY,
+    status_text     VARCHAR(10)     NOT NULL
+);
+
+CREATE TABLE Scheduled_Triggers (
+    id              UNIQUEIDENTIFIER     NOT NULL    PRIMARY KEY,
+    cron_expr       VARCHAR(200)         NOT NULL,
+    last_run        DATETIME             NOT NULL,
+    next_run        DATETIME             NOT NULL,
+    process_path    VARCHAR(250)         NOT NULL,
+    process_status  TINYINT              NOT NULL    FOREIGN KEY REFERENCES Trigger_Status(id),
+    is_git_repo     BIT                  NOT NULL,
+    force_update    BIT                  NOT NULL,
+    blocking        BIT                  NOT NULL
+);
+
+CREATE TABLE Single_Triggers (
+    id              UNIQUEIDENTIFIER     NOT NULL    PRIMARY KEY,
+    last_run        DATETIME             NOT NULL,
+    next_run        DATETIME             NOT NULL,
+    process_path    VARCHAR(250)         NOT NULL,
+    process_status  TINYINT              NOT NULL    FOREIGN KEY REFERENCES Trigger_Status(id),
+    is_git_repo     BIT                  NOT NULL,
+    force_update    BIT                  NOT NULL,
+    blocking        BIT                  NOT NULL
+);
+
+CREATE TABLE Email_Triggers (
+    id              UNIQUEIDENTIFIER     NOT NULL    PRIMARY KEY,
+    email_folder    VARCHAR(250)         NOT NULL,
+    last_run        DATETIME             NOT NULL,
+    process_path    VARCHAR(250)         NOT NULL,
+    process_status  TINYINT              NOT NULL    FOREIGN KEY REFERENCES Trigger_Status(id),
+    is_git_repo     BIT                  NOT NULL,
+    force_update    BIT                  NOT NULL,
+    blocking        BIT                  NOT NULL
+);
+
+CREATE TABLE Credentials (
+    cred_name        VARCHAR(255)     NOT NULL    PRIMARY KEY,
+    cred_username    VARCHAR(255)     NOT NULL,
+    cred_password    VARCHAR(255)     NOT NULL,    
+);
+
+CREATE TABLE Constants (
+    constant_name   VARCHAR(255)     NOT NULL    PRIMARY KEY,
+    constant_value  VARCHAR(1000)    NOT NULL
+);
