c36ea7bc2b159a018297ca71272b247cc10f6e8b
Mathias G <ghbm@aarhus.dk>
2023-07-05 11:33:39 +0200

    Init db button added

--- Settings_tab.py
+++ Settings_tab.py
@@ -11,11 +11,14 @@ class Settings_tab(tkinter.Frame):
         conn_label.pack()
 
         conn_entry = ttk.Entry(self)
-        conn_entry.insert(0, "Driver={ODBC Driver 17 for SQL Server};Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=yes;")
+        conn_entry.insert(0, "Driver={ODBC Driver 17 for SQL Server};Server=SRVSQLHOTEL03;Database=MKB-ITK-RPA;Trusted_Connection=yes;")
         conn_entry.pack(fill='x')
 
         conn_button = ttk.Button(self, text="Connect", command=lambda: connect(parent, conn_entry, conn_button))
         conn_button.pack()
+
+        init_button = ttk.Button(self, text='Initialize Database', command=lambda: initialize_database(parent))
+        init_button.pack()
 
 def connect(app, conn_entry: ttk.Entry, conn_button: ttk.Button):
     conn_string = conn_entry.get()
@@ -23,6 +26,25 @@ def connect(app, conn_entry: ttk.Entry, conn_button: ttk.Button):
         pyodbc.connect(conn_string)
         app.connection_string = conn_string
         conn_button.configure(text="Connected!")
-    except:
+    except Exception as e:
         conn_button.configure(text="Connect")
-        messagebox.showerror("Connection failed", "Connection failed")
+        messagebox.showerror("Connection failed", str(e))
+
+def initialize_database(app):
+    try:
+        conn = pyodbc.connect(app.connection_string)
+    except Exception as e:
+        messagebox.showerror("Connection failed", str(e))
+    
+    with open('SQL/initialize.sql') as sql_file:
+        commands = sql_file.read().split(";")
+        
+    try:
+        for command in commands:
+            if command:
+                conn.execute(command)
+        
+        conn.commit()
+        messagebox.showinfo("Database initialized", "Database tables successfully created.")
+    except Exception as e:
+        messagebox.showerror('Initialization failed', str(e))
