d420da0e3028c1bcc430a662b82c90cce9d31a12
Mathias G <ghbm@aarhus.dk>
2023-07-06 09:13:55 +0200

    Column names changed

--- Trigger_tab.py
+++ Trigger_tab.py
@@ -10,24 +10,24 @@ class Trigger_tab(ttk.Frame):
         self.rowconfigure((0,2,4), weight=6, uniform='a')
         self.rowconfigure((1,3,5), weight=1, uniform='a')
 
-    #Periodic table
-        p_table_frame = tkinter.Frame(self, background='red')
-        p_table_frame.grid(row=0, column=0, sticky='nsew')
+    #Scheduled table
+        s_table_frame = tkinter.Frame(self, background='red')
+        s_table_frame.grid(row=0, column=0, sticky='nsew')
 
-        p_table = create_table(p_table_frame, ('cron', '', 'dig'))
+        s_table = create_table(s_table_frame, ('Process Name', 'Cron', 'Last run', 'Next run', 'Path', 'Status', 'Is GIT?', 'Force update?', 'Blocking?'))
 
-    #Periodic controls
-        p_controls_frame = tkinter.Frame(self, background='blue')
-        p_controls_frame.grid(row=1, column=0, sticky='nsew')
+    #Scheduled controls
+        s_controls_frame = tkinter.Frame(self, background='blue')
+        s_controls_frame.grid(row=1, column=0, sticky='nsew')
 
-        p_button = ttk.Button(p_controls_frame, text='Hej')
-        p_button.pack()
+        s_button = ttk.Button(s_controls_frame, text='Hej')
+        s_button.pack()
     
     #Email table
         e_table_frame = tkinter.Frame(self, background='green')
         e_table_frame.grid(row=2, column=0, sticky='nsew')
 
-        e_table = create_table(e_table_frame, ('Hej', 'med', 'dig'))
+        e_table = create_table(e_table_frame, ('Process Name', 'Folder', 'Last run', 'Path', 'Status', 'Is GIT?', 'Force update?', 'Blocking?'))
     
     #Email controls
         e_controls_frame = tkinter.Frame(self, background='yellow')
@@ -40,7 +40,7 @@ class Trigger_tab(ttk.Frame):
         s_table_frame = tkinter.Frame(self, background='purple')
         s_table_frame.grid(row=4, column=0, sticky='nsew')
 
-        s_table = create_table(s_table_frame, ('Hej', 'med', 'dig'))
+        s_table = create_table(s_table_frame, ('Process Name', 'Last run', 'Next run', 'Path', 'Status', 'Is GIT?', 'Force update?', 'Blocking?'))
     
     #Single controls
         s_controls_frame = tkinter.Frame(self, background='grey')
@@ -55,6 +55,7 @@ def create_table(parent, columns):
     table['columns'] = columns
     for c in table['columns']:
         table.heading(c, text=c, anchor='w')
+        table.column(c, stretch=False)
     
     yscroll = ttk.Scrollbar(parent, orient='vertical', command=table.yview)
     yscroll.pack(side='left', fill='y')
@@ -62,7 +63,7 @@ def create_table(parent, columns):
 
     xscroll = ttk.Scrollbar(parent, orient='horizontal', command=table.xview)
     xscroll.pack(side='bottom', fill='x')
-    table.configure(yscrollcommand=xscroll.set)
+    table.configure(xscrollcommand=xscroll.set)
 
     table.pack(expand=True, fill='both')
 
