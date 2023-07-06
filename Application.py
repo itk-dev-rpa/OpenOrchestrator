378ade6e6d9bac80239183ed5e960eb495160127
Mathias G <ghbm@aarhus.dk>
2023-07-06 11:03:31 +0200

    Name changed

--- Application.py
+++ Application.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
 import Logging_tab, Settings_tab, Trigger_tab
+import pyodbc
 
 class Application(tk.Tk):
-    connection_string = ""
-
     def __init__(self):
+        self.connection_string = ""
+
         tk.Tk.__init__(self)
         self.geometry("850x600")
         style = ttk.Style(self)
@@ -14,15 +15,18 @@ class Application(tk.Tk):
         notebook = ttk.Notebook(self)
         notebook.pack(expand=True, fill='both')
 
-        trig_tab = Trigger_tab.Trigger_tab(notebook)
-        log_tab = Logging_tab.Logging_tab(notebook)
-        set_tab = Settings_tab.Settings_tab(notebook)
+        trig_tab = Trigger_tab.Trigger_tab(notebook, self)
+        log_tab = Logging_tab.Logging_tab(notebook, self)
+        set_tab = Settings_tab.Settings_tab(notebook, self)
 
         notebook.add(trig_tab, text="Triggers")
         notebook.add(log_tab, text="Logs")
         notebook.add(set_tab, text="Settings")
 
         self.mainloop()
-
-if __name__ == '__main__':
+    
+    def connect_to_db(self):
+        return pyodbc.connect(self.connection_string)
+    
+if __name__=='__main__':
     Application()
