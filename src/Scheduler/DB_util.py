6ebf9ad252538d6479c86e6274ba01f5494e77ae
Mathias G <ghbm@aarhus.dk>
2023-08-15 14:18:51 +0200

    Bug fixed

--- src/Scheduler/DB_util.py
+++ src/Scheduler/DB_util.py
@@ -25,7 +25,7 @@ def catch_db_error(func):
     """A decorator that catches errors in SQL queries"""
     def inner(*args, **kwargs):
         try:
-            func(*args, **kwargs)
+            return func(*args, **kwargs)
         except pyodbc.ProgrammingError as e:
             messagebox.showerror("Error", f"Query failed:\n{e}")
     return inner
