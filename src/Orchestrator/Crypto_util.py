2540f6e4c0c80ad8a03079918a8e2bc740c1fe3a
Mathias G <ghbm@aarhus.dk>
2023-08-15 09:07:16 +0200

    Checksum added to encryption

--- src/Orchestrator/Crypto_util.py
+++ src/Orchestrator/Crypto_util.py
@@ -1,4 +1,5 @@
 from cryptography.fernet import Fernet
+import hashlib
 
 _enryption_key = None
 
@@ -10,10 +11,29 @@ def set_key(key:str):
     _enryption_key = key
 
 def encrypt_data(data: str):
-    return Fernet(_enryption_key).encrypt(data.encode()).decode()
+    data = data.decode()
+
+    # Add hash of data to use as checksum
+    hash_data = sha256(data)
+    data = hash_data + data
+    
+    return Fernet(_enryption_key).encrypt(data).decode()
 
 def decrypt_data(data:str):
-    return Fernet(_enryption_key).decrypt(data.encode()).decode()
+    data = data.encode()
+
+    decrypted_data = Fernet(_enryption_key).decrypt(data)
+
+    if len(decrypted_data) < 32:
+        raise ValueError('Decrypted data is too short. The decryption key is not the same as the encryption key.')
+
+    hash_data = decrypted_data[0:32]
+    data = decrypted_data[32:]
+
+    if hash_data == sha256(data):
+        return data.decode()
+    else:
+        raise ValueError('Checksum not correct. The decryption key is not the same as the encryption key.')
 
 def validate_key(key:str):
     try:
@@ -21,3 +41,8 @@ def validate_key(key:str):
         return True
     except:
         return False
+
+def sha256(data:bytes):
+    hasher = hashlib.sha256()
+    hasher.update(data)
+    return hasher.digest()
