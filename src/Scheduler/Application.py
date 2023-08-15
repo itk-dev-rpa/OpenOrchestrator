2584988cf3d6b757c1715c81cc870821e8b619f6
Mathias G <ghbm@aarhus.dk>
2023-08-15 14:20:22 +0200

    Loop logic updated

--- src/Scheduler/Application.py
+++ src/Scheduler/Application.py
@@ -1,12 +1,11 @@
 import tkinter
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import Settings_tab, Run_tab
 
 class Application(tkinter.Tk):
     def __init__(self):
         self.running_jobs = []
         self.running = False
-        self.next_loop = None
 
         super().__init__()
         self.title("OpenOrchestrator - Scheduler")
@@ -22,8 +21,16 @@ class Application(tkinter.Tk):
 
         notebook.add(run_tab, text='Run')
         notebook.add(settings_tab, text="Settings")
+
+        self.protocol('WM_DELETE_WINDOW', self.on_close)
         
         self.mainloop()
+    
+    def on_close(self):
+        if (len(self.running_jobs) == 0 
+            or messagebox.askyesno('Warning', 'Some processes are still running. Closing the scheduler while processes are running will gum up the trigger tables. Are you sure you want to close?')):
+            self.destroy()
+
 
 
 if __name__=='__main__':
