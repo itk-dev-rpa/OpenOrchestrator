feb60d58715fbb4a7b6b6f0e165b91e716df630e
Mathias G <ghbm@aarhus.dk>
2023-11-01 09:44:23 +0100

    Added default value to status

--- OpenOrchestrator/database/queues.py
+++ OpenOrchestrator/database/queues.py
@@ -22,7 +22,7 @@ class QueueElement(Base):
 
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
     queue_name: Mapped[str] = mapped_column(String(100), index=True)
-    status: Mapped[QueueStatus]
+    status: Mapped[QueueStatus] = mapped_column(default=QueueStatus.NEW)
     data: Mapped[Optional[str]]
     reference: Mapped[Optional[str]] = mapped_column(String(100))
     created_date: Mapped[datetime] = mapped_column(default=datetime.now)
