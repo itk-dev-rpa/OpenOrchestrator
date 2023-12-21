214acc266ab670fbf903aaa8dd625aeeb79fd741
Mathias G <ghbm@aarhus.dk>
2023-12-21 15:58:22 +0100

    Added color option to buttons

--- OpenOrchestrator/orchestrator/popups/generic_popups.py
+++ OpenOrchestrator/orchestrator/popups/generic_popups.py
@@ -3,7 +3,7 @@
 from nicegui import ui
 
 
-async def question_popup(question: str, option1: str, option2: str) -> bool:
+async def question_popup(question: str, option1: str, option2: str, color1: str = 'primary', color2: str = 'primary') -> bool:
     """Shows a popup with a question and two buttons with the given options.
     Example:
         result = await question_popup("Do you like candy", "YES!", "Not really")
@@ -12,6 +12,8 @@ async def question_popup(question: str, option1: str, option2: str) -> bool:
         question: The question to display.
         option1: The text on button 1.
         option2: The text on button 2.
+        color1: The color of button 1.
+        color2: The color of button 2.
 
     Returns:
         bool: True if button 1 is clicked, or False if button 2 is clicked.
@@ -19,7 +21,7 @@ async def question_popup(question: str, option1: str, option2: str) -> bool:
     with ui.dialog(value=True).props('persistent') as dialog, ui.card():
         ui.label(question).classes("text-lg")
         with ui.row():
-            ui.button(option1, on_click=lambda e: dialog.submit(True))
-            ui.button(option2, on_click=lambda e: dialog.submit(False))
+            ui.button(option1, on_click=lambda e: dialog.submit(True), color=color1)
+            ui.button(option2, on_click=lambda e: dialog.submit(False), color=color2)
 
         return await dialog
