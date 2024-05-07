855e7a5d7dd30245db15c02b79987f4f7187e0a4
Mathias G <ghbm@aarhus.dk>
2024-05-07 14:20:14 +0200

    Added orange color to pausing triggers

--- OpenOrchestrator/orchestrator/tabs/trigger_tab.py
+++ OpenOrchestrator/orchestrator/tabs/trigger_tab.py
@@ -61,7 +61,7 @@ class TriggerTab():
             "body-cell-Status",
             '''
             <q-td key="Status" :props="props">
-                <q-badge v-if="{Running: 'green', Failed: 'red'}[props.value]" :color="{Running: 'green', Failed: 'red'}[props.value]">
+                <q-badge v-if="{Running: 'green', Pausing: 'orange', Failed: 'red'}[props.value]" :color="{Running: 'green', Pausing: 'orange', Failed: 'red'}[props.value]">
                     {{props.value}}
                 </q-badge>
                 <p v-else>
