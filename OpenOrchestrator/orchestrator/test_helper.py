60234ad837b0cfe18500be83df78a39a719ad5f2
Mathias G <ghbm@aarhus.dk>
2024-07-01 13:03:08 +0200

    Added automation ids to some elements

---
+++ OpenOrchestrator/orchestrator/test_helper.py
@@ -0,0 +1,17 @@
+"""This module contains helper functions for automated tests."""
+
+from nicegui import ui
+
+
+def set_automation_ids(container, prefix: str):
+    """Set automation ids for automated tests.
+    Iterate though all attributes of the container and set 'auto-id'
+    on any ui-elements.
+
+    Args:
+        container: The object that contains a number of ui-elements.
+        prefix: The prefix to add to the automation ids for all elements.
+    """
+    for name, object in container.__dict__.items():
+        if isinstance(object, (ui.button, ui.input, ui.checkbox, ui.number, ui.table, ui.tab)):
+            object.props(f"auto-id={prefix}_{name}")
