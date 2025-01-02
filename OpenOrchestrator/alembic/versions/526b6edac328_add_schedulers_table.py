a819727802c81e9636879ca01439b87fb82df9b7
Mathias G <ghbm@aarhus.dk>
2025-01-02 13:45:16 +0100

    Added database revision

---
+++ OpenOrchestrator/alembic/versions/526b6edac328_add_schedulers_table.py
@@ -0,0 +1,22 @@
+"""Database revision '526b6edac328': Add schedulers table"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '526b6edac328'
+down_revision = 'b67b7649b282'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'Schedulers',
+        sa.Column('machine_name', sa.String(length=100), nullable=False),
+        sa.Column('last_update', sa.DateTime(), nullable=False),
+        sa.Column('latest_trigger', sa.String(length=100), nullable=True),
+        sa.Column('latest_trigger_time', sa.DateTime(), nullable=True),
+        sa.PrimaryKeyConstraint('machine_name')
+    )
