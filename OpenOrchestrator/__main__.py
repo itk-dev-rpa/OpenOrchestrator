293d3fe0db3f0d07d8055544270dcdeb3b1f0b0c
Mathias G <ghbm@aarhus.dk>
2025-06-18 11:14:33 +0200

    Updated cli

--- OpenOrchestrator/__main__.py
+++ OpenOrchestrator/__main__.py
@@ -24,7 +24,7 @@ def main():
     s_parser = subparsers.add_parser("scheduler", aliases=["s"], help="Start the Scheduler application.")
     s_parser.set_defaults(func=scheduler_command)
 
-    u_parser = subparsers.add_parser("upgrade", aliases=["u"], help="Upgrade the database to the newest revision.")
+    u_parser = subparsers.add_parser("upgrade", aliases=["u"], help="Upgrade the database to the newest revision or create a new database from scratch.")
     u_parser.add_argument("connection_string", type=str, help="The connection string to the database")
     u_parser.set_defaults(func=upgrade_command)
 
@@ -54,7 +54,19 @@ def upgrade_command(args: argparse.Namespace):
     """
     confirmation = input("Are you sure you want to upgrade the database to the newest revision? This cannot be undone. (y/n)").strip()
     if confirmation == "y":
-        subprocess.run(["alembic", "-x", args.connection_string, "upgrade", "head"], check=False)
+        # Check if database is already stamped
+        result = subprocess.run(["alembic", "-x", args.connection_string, "current"], check=True, capture_output=True)
+        if result.stdout == b'':
+            response = input("It seems like you are either creating a new database or trying to upgrade a legacy database. Please choose an option: (new/legacy/cancel)").strip()
+            if response == "legacy":
+                subprocess.run(["alembic", "-x", args.connection_string, "stamp", "b67b7649b282"], check=True)
+            elif response != "new":
+                print("Upgrade canceled")
+                return
+
+        subprocess.run(["alembic", "-x", args.connection_string, "upgrade", "head"], check=True)
+        print("Database upgraded to the newest revision!")
+        return
     else:
         print("Upgrade canceled")
 
